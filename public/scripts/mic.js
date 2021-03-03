52385b484cd1affac625f8bb6c8d098efa49af24
riyadhossain1998 <riyad.hossain114@gmail.com>
2021-03-03 15:34:53 -0700

    working v1

--- public/scripts/mic.js
+++ public/scripts/mic.js
@@ -11,6 +11,10 @@ function init() {
     //var gainNode = audioContext.createGain();
     //gainNode.gain.value = 20;
     
+
+
+    document.getElementById("displayText").innerHTML = "Analyzing background noise...";
+
     try
     {
         window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -64,24 +68,79 @@ function init() {
         analyser.fftSize = 1024; // power of 2, between 32 and max unsigned integer
         var bufferLength = analyser.fftSize;
         var freqBinDataArray = new Uint8Array(bufferLength);
-
+        var time = 30;
+        
+        var meanValue = 0;
+        var freqValue = 0;
         var checkAudio = function() {
-            analyser.getByteFrequencyData(freqBinDataArray);
+            try {
+                analyser.getByteFrequencyData(freqBinDataArray);
+            
 
-    
-            document.getElementById("vol").innerHTML = 'Volume: ' + getRMS(freqBinDataArray);
-            document.getElementById("freq").innerHTML = 'Freq Bin: ' + getIndexOfMax(freqBinDataArray);
-            //console.log(freqBinDataArray);
+
+                //document.getElementById("vol").innerHTML = 'Volume: ' + getRMS(freqBinDataArray);
+                //document.getElementById("freq").innerHTML = 'Freq Bin: ' + getIndexOfMax(freqBinDataArray);
+                //console.log(freqBinDataArray);
+                meanValue = getRMS(freqBinDataArray);
+                freqValue = getIndexOfMax(freqBinDataArray);
+            }
+            catch(e) {
+                console.log("Recipe generated!")
+                
+            }            
         }
 
         setInterval(checkAudio, 64);
-        draw();
+        
+        
+
+        var timer = setInterval(function(){
+            if(time <= 0) {
+                clearInterval(timer);
+                analyser = null;
+                audioContext = null;
+                microphone = null;
+            }
+
+            if(time  == 25) {
+                //console.log("At 5s, " + meanValue)
+                document.getElementById("displayText").innerText = "You may start humming! ^_^"
+                selectGreen(meanValue);
+            }
+            else if(time == 20) {
+                // check amplitude
+                //console.log("At 10s, " + meanValue)
+                selectProtein(meanValue);
+            }
+            else if(time == 15) {
+               // console.log("At 15s, " + meanValue)
+                selectTopping(meanValue);
+            }
+            else if(time == 10) {
+                //console.log("At 15s, " + meanValue)
+                selectTopping(meanValue);
+            }
+            else if(time == 5) {
+                //console.log("At 15s, " + meanValue)
+                selectTopping(meanValue);
+            }
+            
+            else if(time == 1) {
+                //console.log("At 30s, " + meanValue)
+                selectDressing(freq);
+            }
+        
+            document.getElementById("time").innerHTML = (30 - time);
+            time -= 1; 
+        }, 1000);
+
     }
 
     
     
     canvasCtx.clearRect(0, 0, 250, 250);
-
+    
+   
     function draw() {
         
         var drawVisual = requestAnimationFrame(draw);
@@ -102,8 +161,6 @@ function init() {
         for(var i = 0; i < bLength; i++) {
             var v = dataArray[i] / 128.0;
             var y = v * 250/2;
-
-            document.getElementById("vval").innerHTML = "val1: " + v + "\nval2: " + y;
 
             if (i == 0) {
                 canvasCtx.moveTo(x, y);
@@ -151,4 +208,22 @@ function getRMS(spectrum) {
 
 function getIndexOfMax(array) {
     return array.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+}
+
+function mean(array) {
+    var avg = 0;
+    for(var i = 0; i < array.length; i++) {
+        avg += array[i];
+    }
+    return(avg/array.legnth);
+}
+
+function mode(array) {
+
+}
+
+function median(array) {
+    
+    return( array[array.length/2] + array[1 + (array.length/2)] )
+
 }
--- public/scripts/recipe.js
+++ public/scripts/recipe.js
@@ -1,70 +1,74 @@
+var greens = ["Lettuce", "Cabbage", "Spinach", "Kale", "Parsley", "Cilantro"];
+var proteins = ["Salmon", "Tuna", "Shrimp", "Chicken", "Turkey", "Tofu", "Boiled Eggs", "Soy Bean Protein"];
+var toppings = ["Walnuts", "Almonds", "Apples", "Broccoli", "Carrots", "Chickpeas", "Croutons",  
+                "Olives", "Kidney Beans", "Cucumbers", "Jalapeno", "Banana Pepper",
+                "Mushrooms", "Red Bell Peppers", "Red Onions", "Roasted Peppers", "Scallions",
+                "Scallions", "Brussels Sprouts", "Edamame", "Bean Sprouts", 
+                "Raisins", "Sunflower Seeds", "Sweet Corn", "Sweet Peas", "Tomatoes", 
+                "Avocado", "Sun-Dried Tomatoes", "Blue Cheese", 
+                "Feta Cheese", "Cheddar Cheese", "Mozzarella"];
+
+var dressing = ["Blue Cheese", "Caesar", "Ranch", "Thousand Island",
+                "Chipotle Ranch", "Lemon Herb", "French", "Roasted Garlic"];
+var vinaigrette = ["Raspberry", "Honey Dijon", "Red Wine", "Balsamic", "Honey Balsamic", "Olive Oil"]
+
+var restrictions = ["Vegan", "Vegetarian", "No Dairy", "Pescetarian", "I only like meat", "Spicy", "Nuts"];
+
+var gVal, pVal, tVal, dVal, bg = 0;
+var numOfToppings = 4;
+
+var preference = {
+    "diet": "",
+    "dairy": "",
+    "nuts": "",
+    "greens": [],
+    "proteins": "",
+    "toppings": [],
+    "dressing": ""
+}
+
 function salad() {
     init();
-    var greens = ["Lettuce", "Spinach", "Kale", "Mix"];
-    var proteins = ["Salmon", "Tuna", "Shrimp", "Chicken", "Turkey", "Tofu", "Boiled Eggs", "Soy Bean Protein"];
-    var toppings = ["Walnuts", "Almonds", "Apples", "Broccoli", "Carrots", "Chickpeas", "Croutons",  
-                    "Olives", "Artichokes", "Kidney Beans", "Cucumbers", "Jalapeno", "Banana Pepper",
-                    "Mushrooms", "Red Bell Peppers", "Red Onions", "Roasted Peppers", "Scallions",
-                    "Scallions", "Brussels Sprouts", "Raisins", "Edamame", "Bean Sprouts", 
-                    "Raisins", "Sunflower Seeds", "Sweet Corn", "Sweet Peas", "Tomatoes", 
-                    "Avocado", "Sun-Dried Tomatoes", "Blue Cheese", 
-                    "Feta Cheese", "Cheddar Cheese", "Mozzarella"];
-    
-    var dressing = ["Blue Cheese", "Caesar", "French", "Thousand Island",
-                    "Roasted Garlic", "Chipotle Ranch", "Lemon Herb", "Ranch", 
-                    "Sesame Ginger", "Olive Oil"];
-    var vinaigrette = ["Raspberry", "Honey Dijon", "Red Wine", "Balsamic", "Honey Balsamic"]
 
-    var restrictions = ["Vegan", "Vegetarian", "No Dairy", "Pescetarian", "I only like meat", "Spicy", "Nuts"];
     //var choice = document.querySelector("option selected");
     //console.log(choice);
-
-    var date = new Date();
-
-    var preference = {
-        "diet": "",
-        "dairy": "",
-        "nuts": ""
-    }
-
-    console.log(date.getHours());
     //document.querySelector(".sing").addEventListener("click", init);
 
     setPref();
-
+    
     function restrictions() {
         // 1-none 2-vegan 3-veg 4-carnivore 5-fish
-        var choice = document.getElementById("inputState");
-        var allergies = document.querySelector(".checkbox-1");
-        console.log(choice);
+       
+        var choice = preference["diet"];
+        
         if(choice == "2") {
             proteins = ["Tofu", "Soy Bean Protein"]
+            dressing.splice(0, 6)
+            toppings.splice(31, 34)
         }
         else if(choice == "3") {
             proteins = ["Boiled Eggs","Tofu", "Soy Bean Protein"]
         }
+        else if(choice == "4") {
+            proteins = ["Chicken", "Turkey"]
+        }
         else if(choice == "5") {
-            proteins = ["Salmon", "Tuna", "Shrimp", "Tofu", "Soy Bean Protein"]
+            proteins = ["Salmon", "Tuna", "Shrimp"]
         }
         else {
             
         }
 
+        if(preference["dairy"]) {
+            dressing.splice(0, 6)
+            toppings.splice(31, 34)
+        }
+        else if(preference["nuts"]) {
+            toppings.splice(0, 2)
+        }
         
     }
 
-
-    function selectGreen() {
-
-    }
-    
-    function selectTopping() {
-    
-    }
-    
-    function selectDressing() {
-        
-    }
     function spicy() {
         var spicy = 5;
        
@@ -82,6 +86,7 @@ function salad() {
             preference["dairy"] = boxValue;
         }
         else {
+
         }
 
     }
@@ -102,8 +107,131 @@ function salad() {
         getCheckBoxValue(2); //dairy
         getDropDownValue(); //diet pref
         console.log(preference);
+        restrictions();
     }
 
+    
+}
+function selectGreen(vol) {
+    
+    bg = vol;
+    if(bg < 5) {
+        gVal = gVal + bg*2;
+    }
+    else if(bg < 10) {
+        gVal = gVal + bg;
+    }
+    
+    if(gVal < 20) {
+        preference["greens"].push("Lettuce");
+    }
+    else if(gVal < 30) {
+        preference["greens"].push("Lettuce");
+        preference["greens"].push("Cabbage");
+    }
+    else if(gVal < 40) {
+        preference["greens"].push("Kale");
+    }
+    else if(gVal < 50) {
+        preference["greens"].push("Spinach");
+    }
+    else {
+        preference["greens"].push(greens[Math.floor(Math.random() * (greens.length))]);   
+    }
+    
+}
+
+function selectProtein(vol) {
+    if(bg < 2) {
+        preference["proteins"] = ""
+        
+    }
+    else {
+        var tmp;
+        if(vol < proteins.length) {
+            tmp = Math.floor(vol); 
+        }
+        else {
+            tmp = Math.floor(Math.random() * proteins.length); 
+        }
+        
+        preference["proteins"] = proteins[tmp];
+    }
+    
+}
+
+function selectTopping(vol) {
+    
+    if(vol > toppings.length) {
+        preference["toppings"].push(toppings[Math.floor(vol-toppings.length)])
+    }
+    else {
+        preference["toppings"].push(toppings[Math.floor(toppings.length - vol)])
+    }
+}
+
+function selectDressing(freq) {
+    
+    var num;
+    if(preference["greens"][0] == "Lettuce" || preference["greens"][1] == "Cabbage") { 
+        if(freq < dressing.length) {
+            num = Math.floor(freq)
+        }
+        else {
+            num = Math.floor(Math.random() * dressing.length)
+        }
+        preference["dressing"] = dressing[num];
+    }
+    else {
+        if(freq < vinaigrette.length) {
+            num = Math.floor(freq);    
+        }
+        else {
+            num = Math.floor(Math.random() * vinaigrette.length)
+        }    
+        preference["dressing"] = vinaigrette[num];
+    }
+    console.log(preference);
+    displayRecipe();
+}
+
+function displayRecipe() {
+    var display = "Start off with some ";
+    if(preference["greens"].length > 1) {
+        for(var i = 0; i < preference["greens"].length; i++) {
+            display += preference["greens"][i] 
+            display += "and ";
+
+        }
+        display += " in a bowl.";
+    }
+    else {
+        display += preference["greens"][0] + "in a bowl. ";
+    }
+    document.getElementById("greens").innerHTML = display;
+    
+    if(preference["proteins"] != "") {
+        display = "Then add some <b>" + preference["proteins"] + "</b>";
+    }
+    
+    document.getElementById("proteins").innerHTML = display;
+    display = "Now add some ";
+    for(var i = 0; i < preference["toppings"].length; i++) {
+        var ig = "";
+        if(i == preference["toppings"].length - 1) {
+            display += "and ";
+            display += preference["toppings"][i];
+        }
+        else {
+            display += preference["toppings"][i] + ", ";    
+        }
+    }
+    
+    document.getElementById("toppings").innerHTML = display;
+    display = "Top it off with some " + preference["dressing"] + "and enjoy!";
+
+    document.getElementById("dressing").innerHTML = display;
+    
     
 }
 
